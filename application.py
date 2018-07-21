4c38aae22b247c51d68d4a0437b86752eddd4a8c
zoskek2003 <Rachel Moon>
2018-07-21 01:20:21 -0400

    commented

--- application.py
+++ application.py
@@ -5,42 +5,46 @@ from flask_socketio import SocketIO, emit
 from flask_session import Session
 import json
 
-
+# Configure session to use filesystem
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-
-
 socketio = SocketIO(app)
 
-# Configure session to use filesystem
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-
-
 Session(app)
 
-# global variables
-channel_list = ['general']
-channels_counter = {'general': 0}
-messages = {}
-message_id = 0
+# set global variables
 
+channel_list = ['general'] # channel list
+channels_counter = {'general': 0} # dictionary to count number of messages in each channel, to store maximum 100 messages per channel
+
+messages = {} # all the messages will be stored in this dictionary
+message_id = 0 #serves as message primary key
+
+
+
+# main page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+# send channel list information to client server
 @app.route("/getchannel")
 def getchannel():
     global channel_list
     return json.dumps(channel_list)
 
+# send messages information to client server
 @app.route("/getmessages")
 def getmessages():
     global messages
     return json.dumps(messages)
 
 
+# when user creates new channel
 @socketio.on("create channel")
 def create_channel(data):
     global channel_list
@@ -48,29 +52,40 @@ def create_channel(data):
 
     new_channel = data["channel"]
 
+    # if user cancels the prompt, return nothing
     if (new_channel == None):
         return;
+
+    # if new channel name already exists in current channels list, re-prompt the user
     elif new_channel in channel_list:
         emit("already existing channel")
+
+    # if new channel name is blank, re-prompt the user
     elif (new_channel.strip() == ""):
         emit("invalid channel name")
+
+    # else add new channel name to global channel list variable, and let client server know
     else:
         channel_list.append(new_channel)
         channels_counter[new_channel] = 0
-        emit("channel updated", {"new_channel": new_channel}, broadcast=True)
+        emit("channel updated", broadcast=True)
 
 
+
+# when user sends a message
 @socketio.on("send message")
 def send(data):
     global messages
     global message_id
     global channels_counter
 
+    # retrieve message data from client server
     session["user_id"] = data["username"]
     timestamp = data["timestamp"]
     current_channel = data["channel"]
     message = data["message"]
 
+    # create a temporary dictionary storing these data
     temp_dict = {
         'username': session["user_id"],
         'channel': current_channel,
@@ -78,11 +93,15 @@ def send(data):
         'message': message
     }
 
+    # put this information into global message dictionary
     messages[message_id] = temp_dict
     current_message_id = message_id
     message_id += 1
+
+    # keep track of number of messages in the current channel
     channels_counter[current_channel] += 1
 
+    # if this channel has reached maximum of 100 messages, delete the oldest message
     if (channels_counter[current_channel] > 100):
         for key in messages:
             if messages[key]["channel"] == current_channel:
@@ -90,14 +109,16 @@ def send(data):
                 channels_counter[current_channel] -= 1
                 break;
 
-
+    # broadcast new message on the client server
     emit("announce message", {"message": message, "key": current_message_id, "user": session["user_id"], "channel": current_channel , "timestamp": timestamp}, broadcast=True)
 
-
+# when user deletes message
 @socketio.on("delete message")
 def delete(data):
     global messages
 
+    # delete message
     key = data["key"]
     del messages[int(key)]
+    
     emit("deleted", {"key": key}, broadcast = True)
--- static/index.js
+++ static/index.js
@@ -1,163 +1,191 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  //when a new user visits the site, prompt the user to type in a username.
-  if (!localStorage.getItem('username')) {
-    var username = prompt("Please create a username to start chatting in ChatterBox!");
-    localStorage.setItem('username', username);
-  };
-  var username = localStorage.getItem('username');
-
-  if(!localStorage.getItem('channel')) {
-    localStorage.setItem('channel', 'general');
-  };
-  var current_channel = localStorage.getItem('channel');
-
-  // set up channel lists and load current channel's messages
-  get_channels();
-  get_messages(current_channel);
-
-
-  // Connect to websocket
-  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
-  // When connected,
-  socket.on('connect',() => {
-    document.querySelectorAll('.channel_link').forEach(channel => {
-      channel.onclick = () => {
-        get_messages(channel.dataset.page);
-        localStorage.setItem("channel", channel.dataset.page);
-        return false;
-      };
-    });
-
-    document.querySelector("#create_channel").onclick = () => {
-      var channel_name = prompt("Name your new channel!");
-      socket.emit('create channel', {"channel": channel_name});
-      return false;
+    // if a new user, prompt the user to type in a username.
+    if (!localStorage.getItem('username')) {
+        var username = prompt("Please create a username to start chatting in ChatterBox!");
+        localStorage.setItem('username', username);
     };
 
-    //delete message
-    document.querySelectorAll("button").forEach(del => {
-      del.onclick = () => {
-        var key = del.parentElement.dataset.key;
-        socket.emit("delete message", {"key": key});
-        return false;
-      };
+    // retrieve username information
+    var username = localStorage.getItem('username');
+
+    //if new user, take the user to general channel
+    if(!localStorage.getItem('channel')) {
+        localStorage.setItem('channel', 'general');
+    };
+
+    // take the user to last visited channel
+    var current_channel = localStorage.getItem('channel');
+
+    // set up channel lists and load current channel's messages
+    get_channels();
+    get_messages(current_channel);
+
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // When connected to socket.io
+    socket.on('connect',() => {
+
+        // when create channel button is clicked, prompt user to create a new channel
+        document.querySelector("#create_channel").onclick = () => {
+            var channel_name = prompt("Name your new channel!");
+            socket.emit('create channel', {"channel": channel_name});
+            return false;
+        };
+
+        // when user clicks a channel, retrieve its message log and store user's current channel information
+        document.querySelectorAll('.channel_link').forEach(channel => {
+            channel.onclick = () => {
+                get_messages(channel.dataset.page);
+                localStorage.setItem("channel", channel.dataset.page);
+                return false;
+            };
+        });
+
+        // when user clicks delete message button, delete the message
+        document.querySelectorAll("button").forEach(del => {
+            del.onclick = () => {
+                var key = del.parentElement.dataset.key;
+                socket.emit("delete message", {"key": key});
+                return false;
+            };
+        });
+    });
+
+    // if new channel name exists, throw error
+    socket.on('already existing channel', () => {
+        var channel_name = prompt("Channel name already exists. Please enter a new channel name!");
+        socket.emit('create channel', {"channel": channel_name});
+    });
+
+    // if new channel name is blank, throw error
+    socket.on('invalid channel name', () => {
+        var channel_name = prompt("You cannot name your channel with blank space. Please type a valid channel name!");
+        socket.emit('create channel', {"channel": channel_name});
+    });
+
+    // if new channel addition is successful, update channel list information
+    socket.on('channel updated', () => {
+        document.location.reload();
     });
 
 
-  });
-
-  socket.on('already existing channel', () => {
-    var channel_name = prompt("Channel name already exists. Please enter a new channel name!");
-    socket.emit('create channel', {"channel": channel_name});
-  });
-
-  socket.on('invalid channel name', () => {
-    var channel_name = prompt("You cannot name your channel with blank space. Please type a valid channel name!");
-    socket.emit('create channel', {"channel": channel_name});
-  });
-
-
-  socket.on('channel updated', data => {
-    var new_channel = data.new_channel;
-    const a = document.createElement('a');
-    a.setAttribute("href", "");
-    a.setAttribute("class", "channel_link")
-    a.setAttribute("data-page", `${new_channel}`);
-    a.innerHTML = new_channel;
-    document.querySelector("#channels").append(a);
-  });
-
-
-  // submit messages
-  document.querySelector("#new-message").onsubmit = () => {
-    var message = document.querySelector("#message").value;
-    var time = new Date();
-    var localtime = time.toLocaleString();
-    var channel = localStorage.getItem("channel");
-    socket.emit('send message', {"message": message, "channel": channel, "username": username, "timestamp": localtime});
-    return false;
-  };
-
-  // When a new message is announced, add the chat message onto the page
-  socket.on('announce message', data => {
-    if (data.channel == localStorage.getItem("channel")) {
-      const li = document.createElement('li');
-      li.setAttribute("data-key", `${data.key}`);
-      li.innerHTML = `<span style = "font-size: 20px"><b> ${data.user} </b></span> <span style = "color: grey">(${data.timestamp})</span><b>:</b> <br> &nbsp;${data.message}`;
-      document.querySelector('#messages').append(li);
-
-      if (data.user == localStorage.getItem("username")) {
-        var del = document.createElement("button");
-        del.setAttribute("class", "delete");
-        del.innerHTML = "Delete";
-        li.append(del);
-      };
-
-      if (document.querySelectorAll("#messages li").length > 100) {
-          messages = document.querySelector("#messages");
-          messages.removeChild(messages.firstChild);
-      };
-
-      document.querySelector("#message").value = '';
+    // when user submits a message, send data to server
+    document.querySelector("#new-message").onsubmit = () => {
+        var message = document.querySelector("#message").value;
+        var time = new Date();
+        var localtime = time.toLocaleString();
+        var channel = localStorage.getItem("channel");
+        socket.emit('send message', {"message": message, "channel": channel, "username": username, "timestamp": localtime});
+        return false;
     };
-  });
 
+    // When a new message is announced, add the chat message onto the page
+    socket.on('announce message', data => {
 
+        // import new message only if new message's channel information is same as user's current channel
+        if (data.channel == localStorage.getItem("channel")) {
+            const li = document.createElement('li');
+            li.setAttribute("data-key", `${data.key}`);
+            li.innerHTML = `<span style = "font-size: 20px"><b> ${data.user} </b></span> <span style = "color: grey">(${data.timestamp})</span><b>:</b> <br> &nbsp;${data.message}`;
+            document.querySelector('#messages').append(li);
 
-  socket.on("deleted", data => {
-    document.querySelector(`[data-key = '${data.key}']`).remove();
-  });
+            // if new message is current user's message, allow delete button to appear
+            if (data.user == localStorage.getItem("username")) {
+                var del = document.createElement("button");
+                del.setAttribute("class", "delete");
+                del.innerHTML = "Delete";
+                li.append(del);
+            };
+
+            // if length of message in the chat exceeds 100, delete the oldest message
+            if (document.querySelectorAll("#messages li").length > 100) {
+                messages = document.querySelector("#messages");
+                messages.removeChild(messages.firstChild);
+            };
+
+            // clear the message form input value
+            document.querySelector("#message").value = '';
+        };
+    });
+
+    // when message is successfully deleted, remove the message from the page
+    socket.on("deleted", data => {
+        document.querySelector(`[data-key = '${data.key}']`).remove();
+    });
 });
 
 
-//function to set up channel lists
+//function to get channel lists from server
 function get_channels() {
-  const request = new XMLHttpRequest();
-  request.open('GET', '/getchannel');
-  request.onload = () => {
-    const channel_list = request.responseText;
-    JSON.parse(channel_list).forEach(channel => {
-      const a = document.createElement('a');
-      a.setAttribute("href", "");
-      a.setAttribute("class", "channel_link")
-      a.setAttribute("data-page", `${channel}`);
-      a.innerHTML = channel;
-      document.querySelector("#channels").append(a);
-    });
-  };
-  request.send();
+
+    var channellist = document.querySelector("#channels");
+
+    // delete old channel lists currently on the page
+    while (channellist.firstChild) {
+        channellist.removeChild(channellist.firstChild);
+    };
+
+    // make an ajax request to retrieve channel list information from server
+    const request = new XMLHttpRequest();
+    request.open('GET', '/getchannel');
+    request.onload = () => {
+        const channel_list = request.responseText;
+
+        // update channel list tab
+        JSON.parse(channel_list).forEach(channel => {
+            const a = document.createElement('a');
+            a.setAttribute("href", "");
+            a.setAttribute("class", "channel_link");
+            a.setAttribute("data-page", `${channel}`);
+            a.innerHTML = channel;
+            document.querySelector("#channels").append(a);
+        });
+    };
+    request.send();
 }
 
+// function to retrieve message logs from a channel
 function get_messages(channel_name) {
-  document.querySelector("#channelname").innerHTML = channel_name;
-  var messagelist = document.querySelector("#messages");
-  while (messagelist.firstChild) {
-    messagelist.removeChild(messagelist.firstChild);
-  };
-  const request = new XMLHttpRequest();
-  request.open('GET', '/getmessages');
-  request.onload = () => {
-    var messages = request.responseText;
-    if (messages.length > 0) {
-      messages = JSON.parse(messages);
-      for (var key in messages) {
-        if (messages[key]["channel"] == channel_name) {
-          const li = document.createElement('li');
-          li.innerHTML = `<span style = "font-size: 20px"><b> ${messages[key]["username"]} </b></span> <span style = "color: grey">(${messages[key]["timestamp"]})</span><b>:</b> <br> &nbsp;${messages[key]["message"]}`;
-          li.setAttribute("data-key", `${key}`);
-          document.querySelector("#messages").append(li);
 
-          if (messages[key]["username"] == localStorage.getItem("username")) {
-            var del = document.createElement("button");
-            del.setAttribute("class", "delete");
-            del.innerHTML = "Delete";
-            li.append(del);
-          };
-        };
-      };
+    // change message header with current channel name
+    document.querySelector("#channelname").innerHTML = channel_name;
+
+    // delete all messages currently on the chat page
+    var messagelist = document.querySelector("#messages");
+    while (messagelist.firstChild) {
+        messagelist.removeChild(messagelist.firstChild);
     };
-  };
-  request.send();
+
+    // make ajax request to retrieve previous messages from server
+    const request = new XMLHttpRequest();
+    request.open('GET', '/getmessages');
+    request.onload = () => {
+        var messages = request.responseText;
+
+        // retrieve only if there is any message saved in the server
+        if (messages.length > 0) {
+            messages = JSON.parse(messages);
+            for (var key in messages) {
+
+                // retrieve only the messages from this specific channel
+                if (messages[key]["channel"] == channel_name) {
+                    const li = document.createElement('li');
+                    li.innerHTML = `<span style = "font-size: 20px"><b> ${messages[key]["username"]} </b></span> <span style = "color: grey">(${messages[key]["timestamp"]})</span><b>:</b> <br> &nbsp;${messages[key]["message"]}`;
+                    li.setAttribute("data-key", `${key}`);
+                    document.querySelector("#messages").append(li);
+
+                    // if user's own message, attach delete button
+                    if (messages[key]["username"] == localStorage.getItem("username")) {
+                        var del = document.createElement("button");
+                        del.setAttribute("class", "delete");
+                        del.innerHTML = "Delete";
+                        li.append(del);
+                    };
+                };
+            };
+        };
+    };
+    request.send();
 }
